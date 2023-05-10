a8203c9fad94a8430848516efdf8c073f68ebf76
guilsk <guilherme.gmdp@gmail.com>
2023-05-10 18:27:03 -0300

    Add project files.

---
+++ Jogo da Forca/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Jogo_da_Forca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label2 = new Label();
+            tb_input_letra = new TextBox();
+            lb_palavra = new Label();
+            lb_boneco = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 64, 0);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(tb_input_letra);
+            panel1.Controls.Add(lb_palavra);
+            panel1.Controls.Add(lb_boneco);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(352, 426);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(0, 354);
+            label2.Name = "label2";
+            label2.Size = new Size(301, 45);
+            label2.TabIndex = 3;
+            label2.Text = "Digite sua tentativa:";
+            // 
+            // tb_input_letra
+            // 
+            tb_input_letra.BackColor = SystemColors.GrayText;
+            tb_input_letra.CharacterCasing = CharacterCasing.Upper;
+            tb_input_letra.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_input_letra.Location = new Point(307, 349);
+            tb_input_letra.Name = "tb_input_letra";
+            tb_input_letra.Size = new Size(32, 50);
+            tb_input_letra.TabIndex = 2;
+            tb_input_letra.KeyPress += tb_input_letra_KeyPress;
+            // 
+            // lb_palavra
+            // 
+            lb_palavra.AutoSize = true;
+            lb_palavra.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_palavra.Location = new Point(39, 270);
+            lb_palavra.Name = "lb_palavra";
+            lb_palavra.Size = new Size(137, 45);
+            lb_palavra.TabIndex = 1;
+            lb_palavra.Text = "_________";
+            // 
+            // lb_boneco
+            // 
+            lb_boneco.AutoSize = true;
+            lb_boneco.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lb_boneco.Location = new Point(3, 0);
+            lb_boneco.Name = "lb_boneco";
+            lb_boneco.Size = new Size(132, 315);
+            lb_boneco.TabIndex = 0;
+            lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||       /|\\\r\n||       / \\\r\n||\r\n|| ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlText;
+            ClientSize = new Size(376, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Jogo da Forca";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label lb_palavra;
+        private Label lb_boneco;
+        private Label label2;
+        private TextBox tb_input_letra;
+    }
+}
---
+++ Jogo da Forca/Form1.cs
@@ -0,0 +1,119 @@
+namespace Jogo_da_Forca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AtivaForca();
+            SelecionaPalavra();
+        }
+
+        char[] palavraSecreta = null;
+
+        private void SelecionaPalavra()
+        {
+            Random random = new Random();
+            int n = random.Next(palavra.Length);
+            palavraSecreta = palavra[n].ToCharArray();
+            for (int i = 0; i < palavraSecreta.Length; i++)
+            {
+                if (i == 0)
+                    lb_palavra.Text = "_";
+                else
+                    lb_palavra.Text += " _";
+            }
+        }
+
+        int erros = 0;
+
+        string[] palavra = { "ABACATE", "ABACAXI", "ACEROLA", "ACAI", "ARACA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJA", "CAJU",
+                "CARAMBOLA", "CUPUACU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MACA", "MANGABA", "MANGA", "MARACUJA", "MURICI",
+                "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA"};
+
+        private void tb_input_letra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string x = lb_palavra.Text.Replace(" ", "");
+            string y = null;
+            char[] p = x.ToCharArray();
+
+            if (tb_input_letra.Text != "")
+            {
+                if (e.KeyChar == (char)13)
+                {
+                    if (ConfereLetras())
+                    {
+                        for (int i = 0; i < palavraSecreta.Length; i++)
+                        {
+                            if (tb_input_letra.Text == palavraSecreta[i].ToString())
+                            {
+                                p[i] = palavraSecreta[i];
+
+                            }
+                        }
+                        for (int j = 0; j < p.Length; j++)
+                            y += p[j].ToString();
+                        for (int i = 0; i < palavraSecreta.Length; i++)
+                        {
+                            if (y[i] == '_' && i == 0)
+                                lb_palavra.Text = "_";
+                            else if (y[i] == '_')
+                                lb_palavra.Text += " _";
+                            else if (y[i] != '_' && i == 0)
+                                lb_palavra.Text = y[i].ToString();
+                            else if (y[i] != '_')
+                                lb_palavra.Text += " " + y[i].ToString();
+
+                            if (y == lb_palavra.Text.Replace(" ", "") && !y.Contains('_'))
+                                GanhouPerdeu(true);
+                        }
+                    }
+                    else
+                    {
+                        erros++;
+                        AtivaForca();
+                    }
+
+                }
+                if (!char.IsControl(e.KeyChar))
+                    e.Handled = true;
+            }
+            if (char.IsNumber(e.KeyChar))
+                e.Handled = true;
+            if (char.IsWhiteSpace(e.KeyChar))
+                e.Handled = true;
+
+
+            tb_input_letra.Text = "";
+        }
+
+        private void AtivaForca()
+        {
+            if (erros == 0) lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       \r\n||       \r\n||       \r\n||\r\n|| \r\n";
+            else if (erros == 1) lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||       \r\n||       \r\n||\r\n|| \r\n";
+            else if (erros == 2) lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||        |\r\n||       \r\n||\r\n|| \r\n";
+            else if (erros == 3) lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||       /|\\\r\n||       \r\n||\r\n|| \r\n";
+            else if (erros == 4) lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||       /|\\\r\n||       /\r\n||\r\n|| \r\n";
+            else if (erros == 5)
+            {
+                lb_boneco.Text = "//==\\\\\r\n||        |\r\n||       O\r\n||       /|\\\r\n||       / \\\r\n||\r\n|| \r\n";
+                GanhouPerdeu(false);
+            }
+        }
+
+        private void GanhouPerdeu(bool x)
+        {
+            if (x) MessageBox.Show("Parabéns você ganhou!");
+            else MessageBox.Show("Que pena, deu forca...");
+            Close();
+        }
+
+        private bool ConfereLetras()
+        {
+            foreach (char c in palavraSecreta)
+                if (c.ToString() == tb_input_letra.Text)
+                    return true;
+            return false;
+        }
+    }
+}
